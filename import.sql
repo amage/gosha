b4c2f48a835bcf826d031262fdcf1abe76bdc486
Iurii Shchekochikhin <amagex@gmail.com>
2019-05-10 06:40:22 +0200

    simple db structure

---
+++ import.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE gosha;
+CREATE TABLE links(
+    id BIGINT,
+    url varchar(65535)
+);
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
+
+var pool *sql.DB
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	type Invertory struct {
@@ -23,6 +30,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	dsn := flag.String("dsn", os.Getenv("DSN"), "connection data source name")
+	flag.Parse()
+	if len(*dsn) == 0 {
+		log.Fatal("missing dsn flag")
+	}
+	var err error
+	pool, err = sql.Open("postgres", *dsn)
+	if err != nil {
+		log.Fatal("unable to use data source name", err)
+	}
+	defer pool.Close()
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
